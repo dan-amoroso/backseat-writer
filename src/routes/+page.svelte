f9a083820485e1b472d4c49d863752686e9c71c0
Dan Amoroso <dan@deadend.dev>
2026-02-02 19:47:57 +0700

    refactor: split debug widget

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -205,36 +205,7 @@
 </div>
 
 {#if editorStateJson}
-  <div class="debug-widget">
-    <button
-      class="debug-toggle"
-      class:debug-toggle-active={debugOpen}
-      on:click={() => (debugOpen = !debugOpen)}
-      aria-label="Toggle editor state"
-    >
-      <svg
-        width="16"
-        height="16"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <polyline points="4 7 4 4 20 4 20 7" />
-        <line x1="9" y1="20" x2="15" y2="20" />
-        <line x1="12" y1="4" x2="12" y2="20" />
-      </svg>
-      <span class="debug-toggle-label">&lt;/&gt;</span>
-    </button>
-    {#if debugOpen}
-      <div class="debug-panel">
-        <div class="debug-panel-header">Editor State</div>
-        <pre class="debug-panel-content">{$editorStateJson}</pre>
-      </div>
-    {/if}
-  </div>
+  <DebugWidget {editorStateJson} />
 {/if}
 
 {#if hasFeedback}
